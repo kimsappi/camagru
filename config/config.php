d5e13e49c8c917ea0e5f2c9b4ca2a3bd5ce55208
kimsappi <sappikim@gmail.com>
2020-04-18 02:16:52 +0300

    Pagination testing + test images for testing

--- config/config.php
+++ config/config.php
@@ -4,4 +4,6 @@ $salt = "camagru.com";
 $admin_username = "admin";
 $admin_password = "Admin123";
 $admin_email = "admin@example.com";
+
+$posts_per_page = 3;
 ?>
--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php
 $head_title = "Camagru";
 require_once($_SERVER["DOCUMENT_ROOT"] . "/require.php");
+require_once($config_path . "config.php");
 require_once($templates_path . "head.php");
 require_once($templates_path . "header.php");
 ?>
@@ -13,17 +14,72 @@ if (!$connection = dbConnect()) {
 	echo "The site appears to be broken! Check back later.";
 	exit();
 }
-$query = "SELECT `id`, `extension` FROM posts ORDER BY `id` DESC";
+
+// Set current page for pagination purposes
+if (isset($_GET['page']) && intval($_GET['page']) && intval($_GET['page'] > 0))
+	$currentPage = $_GET['page'];
+else
+	$currentPage = 1;
+
+// Pagination is defined in config/config.php
+$queryOffset = ($currentPage - 1) * $posts_per_page;
+$queryLimit = $posts_per_page + 1;
+
+$query = <<<EOD
+SELECT `id`, `extension` FROM posts
+	ORDER BY `id` DESC
+	LIMIT $queryOffset, $queryLimit;
+EOD;
+
+$prevPageLink = '';
+$nextPageLink = '';
+if ($currentPage > 1) {
+	$prevPage = $currentPage - 1;
+	$prevPageLink = <<<EOD
+		<div><a href="/?page=$prevPage">Previous page</a></div>
+	EOD;
+}
+
+$imagesFound = 0;
 foreach ($connection->query($query) as $imageData)
 {
+	
 	$fileName = $imageData['id'] . '.' . $imageData['extension'];
 	echo <<<EOD
 		<img src="$uploads_path_url$fileName" class='thumbnail'>
 	EOD;
+	++$imagesFound;
+
+	
+	if ($imagesFound >= $posts_per_page)
+	{
+		$nextPage = $currentPage + 1;
+		$nextPageLink = <<<EOD
+			<div><a href="/?page=$nextPage">Next page</a></div>
+		EOD;
+		break;
+	}
+}
+
+if (!$imagesFound)
+{
+	$query = "SELECT `id` FROM posts LIMIT 1;";
+	if ($connection->query($query))
+	{
+		header('Location: /');
+		exit();
+	}
+	echo "Gallery is currently empty. Why not make the FIRST post?";
 }
 ?>
 
 </div>
+
+<div id='galleryNextPrevNavigation'>
+	<?= $prevPageLink ?>
+	<?= $nextPageLink ?>
+</div>
+
 
 <?php
 require_once($templates_path . "footer.php");
